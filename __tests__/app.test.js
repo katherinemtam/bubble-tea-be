09ebc6907a13500a9286dd463c2b9796b9d247b3
Katherine Tam <katherinemtam@gmail.com>
2021-08-05 23:14:34 -0700

    toppings post route test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,25 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('topping routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  const milkPudding = {
+    name: 'Milk Pudding',
+    description: 'Custard-like in taste and flavor, with a hint of milkiness.',
+    image: 'https://i.ytimg.com/vi/uxtJKbXOlmg/hqdefault.jpg',
+    texture: ['soft'],
+    hasDairy: true,
+    cost: 1,
+  };
+
+  test('creates a topping via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/toppings')
+      .send(milkPudding);
+
+    expect(res.body).toEqual({ id: '1', ...milkPudding });
   });
 });
